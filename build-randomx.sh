019a354026bb23941c763cb06ff5ecd603508ec5
JI-0 <janivkovic11111@gmail.com>
2024-01-29 23:37:48 +0900

    Change RandomX build

--- build-randomx.sh
+++ build-randomx.sh
@@ -4,9 +4,12 @@ cd randomx
 mkdir lib
 git clone https://github.com/tevador/RandomX RandomX
 # git checkout "102f8ac"
+mv RandomX/src/configuration.h RandomX/src/configuration.h.old
 cp configuration.h RandomX/src/configuration.h
 cd RandomX
 mkdir build
 cd build
 cmake -DARCH=native ..
 make && cp librandomx.a ../../lib/
+cd ..
+mv src/configuration.h.old src/configuration.h
